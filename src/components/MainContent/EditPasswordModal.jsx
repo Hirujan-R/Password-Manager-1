b0a639eeb99ed02799679580c581fc14fd519016
Hirujan Rangaraj <70657769+Hirujan-R@users.noreply.github.com>
2024-08-08 14:58:43 +0100

    Fixed bug with edit password inputs that didn't allow to type into inputs

--- src/components/MainContent/EditPasswordModal.jsx
+++ src/components/MainContent/EditPasswordModal.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import Modal from '../Modal.jsx';
 import { randomisePassword } from '../../utils/PasswordUtils.jsx';
 
 
 
+
 function EditPasswordModal({ show, onHide, handleEditPassword, password}) {
+
+  const [currentServiceName, setCurrentServiceName] = useState(password.name);
+  const [currentPasswordValue, setCurrentPasswordValue] = useState(password.password);
+
+  const handleServiceNameChange = (e) => {
+    setCurrentServiceName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setCurrentPasswordValue(e.target.value);
+  };
 
   const modalTitle = (
     "Change Password"
@@ -13,9 +25,9 @@ function EditPasswordModal({ show, onHide, handleEditPassword, password}) {
 
   const bodyContent = (
     <div>
-      <p>Service Name: <input id='serviceInput' value={password.name}></input></p>
+      <p>Service Name: <input id='serviceInput' onChange={handleServiceNameChange} value={currentServiceName}></input></p>
       <p>Password: 
-        <input id='passwordInput' value={password.password}></input> 
+        <input id='passwordInput' onChange={handlePasswordChange} value={currentPasswordValue}></input> 
         <Button onClick={() => randomisePassword('passwordInput')}>Randomise</Button>
       </p>
     </div>
