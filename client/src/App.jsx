b470060f9633477c5b3adea512d985dc94ab4270
Hirujan Rangaraj <70657769+Hirujan-R@users.noreply.github.com>
2024-08-16 15:32:27 +0100

    Moved code to client folder so that client and server code is separated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Alert } from 'react-bootstrap';
 import React, { useState } from 'react';
-import MainPage from './components/MainPage/MainPage'
+import MainPage from './components/MainPage/MainPage';
 import { LoginPage } from './components/LoginPage/LoginPage';
 import { RegistrationPage } from './components/RegistrationPage/RegistrationPage'
 
 const App = () => {
   
   return (
-    <RegistrationPage />
+    <MainPage />
     
   );
 };
--- client/src/components/MainPage/MainContent/MainContent.jsx
+++ client/src/components/MainPage/MainContent/MainContent.jsx
@@ -1,5 +1,5 @@
 import PasswordTable from './PasswordTable.jsx';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function MainContent({passwords, setPasswords, query, openPasswordEdittedAlert, openPasswordDeletedAlert}) {
